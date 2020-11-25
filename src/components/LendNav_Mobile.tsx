0d5ac5e8c7c84102db72767e461b0a7a818f5b26
UnsignedInt8 <unsignedint8@gmail.com>
2020-11-25 14:52:21 +0800

    [Client] Lend page supports mobile style

---
+++ src/components/LendNav_Mobile.tsx
@@ -0,0 +1,34 @@
+import "./LendNav_Mobile.scss";
+
+import React, { Component } from "react";
+
+import { Link } from "react-router-dom";
+import i18n from "../i18n";
+
+interface Props {
+  type: "lend" | "borrow";
+}
+
+class LendNav_Mobile extends Component<Props, {}> {
+  render() {
+    return (
+      <div className="lendnav_mobile">
+        <div>
+          <Link className={`${this.props.type === "lend" ? "active" : ""}`} to="/lend">
+            {i18n.t("lend_title")}
+          </Link>
+          <span>|</span>
+          <Link className={`${this.props.type === "borrow" ? "active" : ""}`} to="/borrow">
+            {i18n.t("loan_title")}
+          </Link>
+        </div>
+
+        <div>
+          <Link to="/history">{i18n.t("header_history")}</Link>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default LendNav_Mobile;
