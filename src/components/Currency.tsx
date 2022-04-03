badcda53076c01715d0f32ef4293ff02a844bd8d
Tianyou Luo <luotywork@gmail.com>
2022-04-03 19:56:04 -0400

    Deployment for Diners Club

--- src/components/Currency.tsx
+++ src/components/Currency.tsx
@@ -3,12 +3,16 @@ import eth from "../assets/crypto/eth.svg";
 import dai from "../assets/crypto/dai.svg";
 import usdc from "../assets/crypto/usdc.svg";
 import usdt from "../assets/crypto/usdt.svg";
+import sgc from "../assets/crypto/sgc.svg";
+import xbtc from "../assets/crypto/xbtc.svg";
 
 const icons = new Map([
   ["eth", eth],
   ["dai", dai],
   ["usdc", usdc],
   ["usdt", usdt],
+  ["sgc", sgc],
+  ["xbtc", xbtc],
 ]);
 
 interface IProps {
